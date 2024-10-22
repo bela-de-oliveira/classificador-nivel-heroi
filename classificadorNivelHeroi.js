06d59042952c26cc07cbf6d705b119390e911697
Isabela de Oliveira <belaraolive@gmail.com>
2024-10-22 20:47:14 -0300

    "Melhorando o JS"

--- classificadorNivelHeroi.js
+++ classificadorNivelHeroi.js
@@ -20,4 +20,4 @@ if(xpHeroi < 1000){
 	nivelHeroi = "Radiante"
 }
 
-console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`)
